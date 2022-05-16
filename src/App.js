64667a373bab1a6b698a20d177dd1077ceb6f586
Beom-J <beom00032@gmail.com>
2022-05-17 01:16:48 +0900

    feat: Counter 컴포넌트 -> CounterContainer 로 교체

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import Counter from './components/counter';
 import Todos from './components/todos';
+import CounterContainer from './containers/counterContainer';
 
 function App() {
   return (
     <div className="App">
-      <Counter number={0} />
+      <CounterContainer />
       <hr />
       <Todos />
     </div>
