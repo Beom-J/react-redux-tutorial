f742310bda5fda8bad9774a972b44da228f11911
Beom-J <beom00032@gmail.com>
2022-05-17 01:16:21 +0900

    feat: redux 사용 위해 index 에 store 생성

--- src/index.js
+++ src/index.js
@@ -3,12 +3,21 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Provider } from 'react-redux';
+import rootReducer from './modules';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import { legacy_createStore as createStore } from 'redux';
+
+// TODO 이 부분 legacy 말고 그냥 createStoreHook 으로 어떻게 하는지 수정하기
+const store = createStore(rootReducer, composeWithDevTools());
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>,
 );
 
 // If you want to start measuring performance in your app, pass a function
