7ad78cb3a25c71f00e7d3b88611052a752e3ca9a
Beom-J <beom00032@gmail.com>
2022-06-20 02:00:43 +0900

    fix: redux-actions 로 코드 수정

--- src/modules/counter/index.js
+++ src/modules/counter/index.js
@@ -1,10 +1,14 @@
+import { createAction, handleActions } from 'redux-actions';
+
 // 액션 타입 정의: 모듈 이름/액션 이름
 const INCREASE = 'counter/INCREASE';
 const DECREASE = 'counter/DECREASE';
 
 // 액션 생성함수 만들기
-export const increase = () => ({ type: INCREASE });
-export const decrease = () => ({ type: DECREASE });
+// export const increase = () => ({ type: INCREASE });
+// export const decrease = () => ({ type: DECREASE });
+export const increase = createAction(INCREASE);
+export const decrease = createAction(DECREASE);
 
 // 초기 상태 객체
 const initialState = {
@@ -12,19 +16,26 @@ const initialState = {
 };
 
 // reducer 함수
-function counter(state = initialState, action) {
-  switch (action.type) {
-    case INCREASE:
-      return {
-        number: state.number + 1,
-      };
-    case DECREASE:
-      return {
-        number: state.number - 1,
-      };
-    default:
-      return state;
-  }
-}
+// function counter(state = initialState, action) {
+//   switch (action.type) {
+//     case INCREASE:
+//       return {
+//         number: state.number + 1,
+//       };
+//     case DECREASE:
+//       return {
+//         number: state.number - 1,
+//       };
+//     default:
+//       return state;
+//   }
+// }
+const counter = handleActions(
+  {
+    [INCREASE]: (state, action) => ({ number: state.number + 1 }),
+    [DECREASE]: (state, action) => ({ number: state.number - 1 }),
+  },
+  initialState,
+);
 
 export default counter;
